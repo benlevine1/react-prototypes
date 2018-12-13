fcd46773097688abaca5cce57f55216273ac3343
benlevine1 <benlevine1@gmail.com>
2018-12-12 17:57:14 -0800

    functional components prototype complete

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,30 @@
+import React from 'react'
+import Table from './table'
 
+const students = [
+    {
+        name: 'Goofy',
+        course: 'Canine Behavior',
+        grade: '99'
+    },
+    {
+        name: 'Scrooge McDuck',
+        course: 'Finance',
+        grade: '100'
+    },
+    {
+        name: 'Ariel',
+        course: 'Marine Bio',
+        grade: '95'
+    }
+]
+
+export default ()=>{
+    return (
+    <div className = 'container'>
+        <h1>Student Grade Table</h1>
+        <Table data = {students}/>
+    </div>
+    )
+}
 
